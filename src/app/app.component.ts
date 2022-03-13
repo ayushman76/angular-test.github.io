4d7116dce4bc1ad89027d1333ffd6d4ec63b38d8
Ayushman Chaurasia <ayushman.chaurasia@aspiresys.com>
2022-03-13 11:32:37 +0530

    twelv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-website';
+  Pwa: any;
+  installPwa(): void {
+    this.Pwa.promptEvent.prompt();
+  }
 }
